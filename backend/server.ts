a8849a6145caa07e811282455e55f8cb2100c418
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2024-04-15 15:04:13 +0200

    test

--- backend/server.ts
+++ backend/server.ts
@@ -16,6 +16,7 @@ const app:Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./'));
 app.use(helmet({
   crossOriginResourcePolicy: { policy: 'cross-origin' },
   contentSecurityPolicy: false,
