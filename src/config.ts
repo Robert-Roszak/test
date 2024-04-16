4d394bdb4fc59743ceb1633b78546160543645db
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2024-04-16 15:22:09 +0200

    test

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = (process.env.NODE_ENV === 'production') ? 'https://lensshop-backend.vercel.app/' : 'http://localhost:8000/api';
+export const API_URL = (process.env.NODE_ENV === 'production') ? 'https://lensshop-backend.vercel.app/api' : 'http://localhost:8000/api';
 export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/photos' : 'http://localhost:8000/photos';
